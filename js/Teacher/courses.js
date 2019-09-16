7711248dc56924b2335ee0181053c43253f11da6
Munswarim <munswarim@yahoo.com>
2019-09-16 06:13:46 +0600

    improved UI behaviour for JS

--- js/Teacher/courses.js
+++ js/Teacher/courses.js
@@ -2,44 +2,51 @@
 const init = function(e)
 	{
 
-		let data = [];
-		data[0] = localStorage.getItem('course-code');
-		data[1] = localStorage.getItem('credit-hr');
-		data[2] = localStorage.getItem('course-title');
-		data[3] = localStorage.getItem('joining-code');
+		let course_code=[];
+		let credit_hr=[];
+		let course_title=[];
+		let joining_code=[];
 
-		if(data[0] != '' && data[1] != 'none' && data[2] != '' && data[3] != '')
+		course_code = JSON.parse(localStorage.getItem('course-code'));
+		credit_hr = JSON.parse(localStorage.getItem('credit-hr'));
+		course_title = JSON.parse(localStorage.getItem('course-title'));
+		joining_code = JSON.parse(localStorage.getItem('joining-code'));
+
+		if(course_code != null && credit_hr != null && course_title != null && joining_code != null)
 		{
-			let td = [];
-
-			for(var i=0; i<4; i++)
-			{	
-				td[i] = document.createElement('td');
-				if(i!=2)
-					td[i].appendChild(document.createTextNode(data[i]));
-				else
-				{
-					td[i].innerHTML = '<a href ="#">' + data[i] + '</a>';
-				}
-			}
-
-			td[0].id = 'course-code';
-			td[1].id = 'credit-hr';
-			td[2].id = 'course-title';
-			td[3].id = 'joining-code';
-
-			let tr = document.createElement('tr');
-
-			for(var i=0; i<4; i++)
-			{
-				tr.appendChild(td[i]);
-			}
-
+			let td=[];
+			let tr=[];
 			let table = document.getElementById("table");
-			table.appendChild(tr);
+			for(var i=0; i<course_title.length; i++)
+			{
+				td[i]=[];
+				for(var j=0; j<4; j++)
+				{	
+					td[i][j] = document.createElement('td');
+				}
+				
+				td[i][0].appendChild(document.createTextNode(course_code[i]));
+				td[i][1].appendChild(document.createTextNode(credit_hr[i]));
+				td[i][2].innerHTML = '<a href ="#">' + course_title[i] + '</a>';
+				td[i][3].appendChild(document.createTextNode(joining_code[i]));
+
+				td[i][0].id = 'course-code';
+				td[i][1].id = 'credit-hr';
+				td[i][2].id = 'course-title';
+				td[i][3].id = 'joining-code';
+				console.log(td[i]);
+				tr[i] = document.createElement('tr');
+
+				for(var j=0; j<4; j++)
+				{
+					tr[i].appendChild(td[i][j]);
+				}
+				table.appendChild(tr[i]);
+			}
 		}
 	};
 document.addEventListener('DOMContentLoaded',function()
 		{
+			console.log('DOM content loaded');
 			init();
 		});
--- js/Teacher/new_course.js
+++ js/Teacher/new_course.js
@@ -8,15 +8,54 @@ const init = function(e){
 		let course_code = document.getElementById("Course code").value;
 		let credit_hr = document.getElementById("Credit hr").value;
 		let joining_code = document.getElementById("Joining code").value;
-		localStorage.setItem('course-title', course_title);
-		localStorage.setItem('course-code', course_code);
-		localStorage.setItem('credit-hr', credit_hr);
-		localStorage.setItem('joining-code', joining_code);
+		
+		if(course_title == '' || course_code == '' || credit_hr == 'none' || joining_code == '')
+		{
+			window.document.location = './courses.html';
+			return;
+		}
+
+		let course_title_arr;
+		let course_code_arr;
+		let credit_hr_arr;
+		let joining_code_arr;
+
+		try
+		{
+			course_title_arr=JSON.parse(localStorage.getItem('course-title'));
+			course_code_arr=JSON.parse(localStorage.getItem('course-code'));
+			credit_hr_arr=JSON.parse(localStorage.getItem('credit-hr'));
+			joining_code_arr=JSON.parse(localStorage.getItem('joining-code'));
+			
+			course_title_arr[course_title_arr.length]=course_title;
+			course_code_arr[course_code_arr.length]=course_code;
+			credit_hr_arr[credit_hr_arr.length]=credit_hr;
+			joining_code_arr[joining_code_arr.length]=joining_code;
+		}
+		catch(err)
+		{
+			course_title_arr=[];
+			course_code_arr=[]
+			credit_hr_arr=[];
+			joining_code_arr=[];
+			
+			course_title_arr[0]=course_title;
+			course_code_arr[0]=course_code;
+			credit_hr_arr[0]=credit_hr;
+			joining_code_arr[0]=joining_code;
+		}
+		finally
+		{
+			localStorage.setItem('course-title', JSON.stringify(course_title_arr));
+			localStorage.setItem('course-code', JSON.stringify(course_code_arr));
+			localStorage.setItem('credit-hr', JSON.stringify(credit_hr_arr));
+			localStorage.setItem('joining-code', JSON.stringify(joining_code_arr));
+			window.document.location = '../Teacher/courses.html';
+		}
 		/*console.log(course_title);
 		console.log(course_code);
 		console.log(credit_hr);
 		console.log(joining_code);*/
-		window.document.location = '../Teacher/courses.html';
 	});
 };
 document.addEventListener('DOMContentLoaded', function(){
